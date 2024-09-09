f492d7a19bf437b1e6b717d1d7f728b7a16db93d
Jacob (╯°o°）╯︵ ┻━┻ <jacob@otonomos.com>
2024-09-09 16:26:50 -0700

    Removed more instances of 'any' from codebase, also updated README more

--- src/app/api/_apiFactory/nextResponseHandler.ts
+++ src/app/api/_apiFactory/nextResponseHandler.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server"
 /* Handles API responses by returning a `NextResponse` object with an appropriate status code.
  * This function is designed to return a successful response with a specified status code,
  * or handle error responses if necessary. The status code for successful responses defaults to 200. */
-export function NextResponseHandler<any>(
+export function NextResponseHandler(
   response: any,
   successStatusCode: number = 200 // Default to 200 if not specified
 ): NextResponse {
